59f12f7512480dcf92f998f88c3410903636e750
Сергей <smolanovsergey@gmail.com>
2023-02-27 16:59:24 +0200

    main page

--- wp-content/themes/akymed/science.php
+++ wp-content/themes/akymed/science.php
@@ -27,9 +27,115 @@ $post_id = get_the_ID();
                 </h1>
             </div>
         </section>
+        <section class="two-column">
+            <div class="two-column__container main-container">
+                <h2 class="two-column__title section-title">
+                    Concentration and Motility Analysis
+                </h2>
+                <p class="two-column__subtitle section-subtitle">QualiSperm uses proprietary semen analysis algorithms based on fundamentally new technologies rendering reproductive analysis at all levels of concentration. Our Trajectory algorithm and Image Correlation Analysis Method, combined with advanced Digital Sperm Washing, delivers fast and accurate results.</p>
+            </div>
+            <div class="two-column__list main-container">
+                <div class="two-column__item">
+                    <h3 class="two-column__item-title">
+                        Image Recognition
+                    </h3>
+                    <div class="two-column__item-desc">
+                        <p>Using a larger field, QualiSperm is able to determine more sperms per unit area than any other CASA software. Its advanced image recognition algorithm captures up to 1000 sperms per image within only 4 seconds and further increases accuracy by scanning as many individual fields as are needed to reach a statistically relevant number.</p>
+                    </div>
+                </div>
+                <div class="two-column__item">
+                    <h3 class="two-column__item-title">
+                        Trajectory Analysis
+                    </h3>
+                    <div class="two-column__item-desc">
+                        <p>Using a larger field, QualiSperm is able to determine more sperms per unit area than any other CASA software. Its advanced image recognition algorithm captures up to 1000 sperms per image within only 4 seconds and further increases accuracy by scanning as many individual fields as are needed to reach a statistically relevant number.</p>
+                    </div>
+                </div>
+                <div class="two-column__item">
+                    <h3 class="two-column__item-title">
+                        Digital Sperm Washing
+                    </h3>
+                    <div class="two-column__item-desc">
+                        <p>Our unique Digital Sperm Washing (DSW) algorithm significantly improves discrimination of non sperm objects within the captured images. This technology obliterates time consuming manual post processing and delivers precise results measuring concentration and motility.</p>
+                    </div>
+                </div>
+                <div class="two-column__item">
+                    <h3 class="two-column__item-title">
+                        Image Correlation Analysis
+                    </h3>
+                    <div class="two-column__item-desc">
+                        <p>Using a larger field, QualiSperm is able to determine more sperms per unit area than any other CASA software. Its advanced image recognition algorithm captures up to 1000 sperms per image within only 4 seconds and further increases accuracy by scanning as many individual fields as are needed to reach a statistically relevant number.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="two-column-img">
+            <div class="two-column-img__container main-container">
+                <div class="two-column-img__img">
+                    <img src="<?php echo get_template_directory_uri() . '/img/templates/two-column-img/img.png'?>" alt="">
+                </div>
+            </div>
+            <div class="two-column__container main-container">
+                <h2 class="two-column__title section-title">
+                    Morphology
+                </h2>
+                <p class="two-column__subtitle section-subtitle">The sperm morphology module of QualiSperm is primarily based on mathematical morphology segmentation techniques. A dynamic local threshold operation transforms the grayscale microscope image into a binary matrix.</p>
+            </div>
+            <div class="two-column__list main-container">
+                <div class="two-column__item">
+                    <div class="two-column__item-desc">
+                        <p>Several morphological shape-based filters are applied to identify various, specific sperm parts: head, acrosome, mid-piece and tail. The identified (segmented) sperm parts are then quantified in terms of purely geometric parameters such as length, width, area, angles, symmetry, perimeter and roughness.</p>
+                    </div>
+                </div>
+                <div class="two-column__item">
+                    <div class="two-column__item-desc">
+                        <p>Sperms are classified against the WHO and Kruger sperm normality criteria. For instance, a head shape is classified as tapered if the ratio between head length and head width is higher than its corresponding normality interval.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
         <section class="image-small">
             <div class="image-small__bg">
                 <img src="<?php echo get_template_directory_uri() . '/img/templates/image-small/bg.jpg'?>" alt="">
+            </div>
+        </section>
+        <section class="publication">
+            <div class="publication__container main-container">
+                <h2 class="publication__title section-title">
+                    Publications
+                </h2>
+                <div class="publication__list">
+                    <div class="publication__item">
+                        <a href="#"><h3 class="publication__item-title">2020 – Chicken seminal fluid lacks CD9- and CD44-bearingextracellular vesicles</h3></a>
+                        <p>Alvarez-Rodriguez M, Ntzouni M, Wright D, et al.</p>
+                        <p>Reprod Dom Anim. 2020; 55: 293–300</p>
+                    </div>
+                    <div class="publication__item">
+                        <a href="#"><h3 class="publication__item-title">2019 – Assessment of Oligo-Chitosan Biocompatibility toward Human Spermatozoa</h3></a>
+                        <p>Schimpf U, Nachmann G, Trombotto S, Houska P, Yan H, Björndahl L, Crouzier T.</p>
+                        <p>ACS Applied Materials & Interfaces 2019; 11:50, 46572-46584</p>
+                    </div>
+                    <div class="publication__item">
+                        <a href="#"><h3 class="publication__item-title">2019 – Assessment of Oligo-Chitosan Biocompatibility toward Human Spermatozoa</h3></a>
+                        <p>Schimpf U, Nachmann G, Trombotto S, Houska P, Yan H, Björndahl L, Crouzier T.</p>
+                        <p>ACS Applied Materials & Interfaces 2019; 11:50, 46572-46584</p>
+                    </div>
+                    <div class="publication__item">
+                        <a href="#"><h3 class="publication__item-title">2019 – Assessment of Oligo-Chitosan Biocompatibility toward Human Spermatozoa</h3></a>
+                        <p>Schimpf U, Nachmann G, Trombotto S, Houska P, Yan H, Björndahl L, Crouzier T.</p>
+                        <p>ACS Applied Materials & Interfaces 2019; 11:50, 46572-46584</p>
+                    </div>
+                    <div class="publication__item">
+                        <a href="#"><h3 class="publication__item-title">2019 – Assessment of Oligo-Chitosan Biocompatibility toward Human Spermatozoa</h3></a>
+                        <p>Schimpf U, Nachmann G, Trombotto S, Houska P, Yan H, Björndahl L, Crouzier T.</p>
+                        <p>ACS Applied Materials & Interfaces 2019; 11:50, 46572-46584</p>
+                    </div>
+                    <div class="publication__item">
+                        <a href="#"><h3 class="publication__item-title">2019 – Assessment of Oligo-Chitosan Biocompatibility toward Human Spermatozoa</h3></a>
+                        <p>Schimpf U, Nachmann G, Trombotto S, Houska P, Yan H, Björndahl L, Crouzier T.</p>
+                        <p>ACS Applied Materials & Interfaces 2019; 11:50, 46572-46584</p>
+                    </div>
+                </div>
             </div>
         </section>
         <?php get_template_part( 'template-parts/content', 'request' ); ?>
