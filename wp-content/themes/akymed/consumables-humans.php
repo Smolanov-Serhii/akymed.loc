b18073fed0313501d2f8dbb02008d1604aa3a5f9
Сергей <smolanovsergey@gmail.com>
2023-03-02 13:57:22 +0200

    main page

---
+++ wp-content/themes/akymed/consumables-humans.php
@@ -0,0 +1,35 @@
+<?php
+/* Template Name: Consumables - Humans */
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package akymed
+ */
+
+get_header();
+$post_id = get_the_ID();
+?>
+	<main id="main" class="main">
+        <section class="text-title">
+            <div class="text-title__bg">
+                <img src="<?php echo get_template_directory_uri() . '/img/templates/text-title/bg.svg'?>" alt="">
+            </div>
+            <div class="text-title__container main-container">
+                <h1 class="text-title__title banner-title">
+                    Concentration and <br>Motility disposables
+                </h1>
+            </div>
+        </section>
+
+        <?php get_template_part( 'template-parts/content', 'request' ); ?>
+
+	</main>
+<?php
+get_footer();
